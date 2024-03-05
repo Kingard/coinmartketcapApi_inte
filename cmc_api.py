1ca9ad89f224fed78fe2e74e5cb7bbe543aa7099
kingard <kuzkingard@gmail.com>
2024-03-06 00:04:48 +0100

    added function to get latest airdrops

--- cmc_api.py
+++ cmc_api.py
@@ -19,6 +19,7 @@ class CMC:
         self.session = Session()
         self.session.headers.update(self.headers)
 
+    # getAllcoins returns all tokens listed on cmc till last recorded timestamp
     def getAllcoins(self):
         endPoint = '/v1/cryptocurrency/map'
         url = self.base_url+endPoint
@@ -26,7 +27,8 @@ class CMC:
         allCoins = pp(result.json()['data'])
         
         return allCoins
-    
+   
+    # getLatestQuote returns latest quote price of selected token/coin
     def getLatestQuote(self,symbol):
         endPoint = '/v2/cryptocurrency/quotes/latest'
         url = self.base_url + endPoint
@@ -40,13 +42,25 @@ class CMC:
 
         return latest_quote
 
+    # getLatestAirdrop returns latest airdops ONGOING, ENDED, UPCOMING
+    def getLatestAirdrop(self,status):
+        endpoint = '/v1/cryptocurrency/airdrops'
+        url = self.base_url+endpoint
+        parameters = {
+            'status':status.upper()
+        }
+        r = self.session.get(url=url,params=parameters)
+        r = r.json()
+        return pp(r)
+        
+
 
 
 
 cmc = CMC(secret.api_key)
 #print(cmc.getAllcoins())
-print(cmc.getLatestQuote('ETH'))
-
+#print(cmc.getLatestQuote('ETH'))
+print(cmc.getLatestAirdrop('ongoing'))
 #print(pp(request.json()['data'][-1]))
 
 
